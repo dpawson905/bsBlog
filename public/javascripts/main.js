3e1bde74fb2a4ff1222fd7876e95dc40c11a53f8
Darrell Pawson <dpawson905@gmail.com>
2019-05-29 16:31:12 -0400

    make more style changes for toasts

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,52 @@
 /*global $:true, jQuery:true */
 
 function checkPasswordMatch() {
-  var password = $('#password').val();
-  var confirmPassword = $('#password2').val();
+  var password = $("#password").val();
+  var confirmPassword = $("#password2").val();
 
   if (password != confirmPassword)
-    $('#checkPw').addClass('form-error').removeClass('form-success').html('Passwords do not match!');
+    $("#checkPw")
+      .addClass("form-error")
+      .removeClass("form-success")
+      .html("Passwords do not match!");
   else
-    $('#checkPw').addClass('form-success').removeClass('form-error').html('Passwords match.');
+    $("#checkPw")
+      .addClass("form-success")
+      .removeClass("form-error")
+      .html("Passwords match.");
 }
 
-$(document).ready(function () {
-  $(document).scroll(function () {
-    var $nav = $(".fixed-top");
-    var $nav1 = $(".fixed-bottom");
-    var $navHeight = $(".header-img");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $navHeight.height() - 50);
-    $nav1.toggleClass('scrolled', $(this).scrollTop() > $nav1.height());
+var $nav = $(".fixed-top");
+var $nav1 = $(".fixed-bottom");
+var $navHeight = $(".header-img");
+
+$(document).ready(function() {
+  $(document).scroll(function() {
+    $nav.toggleClass(
+      "scrolled",
+      $(this).scrollTop() > $navHeight.height() - 50
+    );
+    $nav1.toggleClass("scrolled", $(this).scrollTop() > $nav1.height());
   });
 
-  $('#password2, #checkPw').keyup(checkPasswordMatch);
+  $("#password2, #checkPw").keyup(checkPasswordMatch);
 
-  // window.setTimeout(function () {
-  //   $('.toast')
-  //     .fadeTo(100, 0)
-  //     .slideUp(500, function () {
-  //       $(this).remove();
-  //     });
-  // }, 10000);
+  function getDocHeight() {
+    var D = document;
+    return Math.max(
+      D.body.scrollHeight,
+      D.documentElement.scrollHeight,
+      D.body.offsetHeight,
+      D.documentElement.offsetHeight,
+      D.body.clientHeight,
+      D.documentElement.clientHeight
+    );
+  }
+
+  window.setTimeout(function () {
+    $('.toast')
+      .slideUp(500, function () {
+        $(this).remove();
+      });
+  }, 10000);
 });
