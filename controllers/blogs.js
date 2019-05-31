719decad92ce82c51fb09f8b953d49114db7f370
Darrell Pawson <dpawson905@gmail.com>
2019-05-30 21:00:24 -0400

    work on blogs landing page

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,14 @@
+const Blog = require('../models/blog');
+const User = require('../models/user');
+
 module.exports = {
   async getBlogs(req, res, next) {
-    res.send('This is the placeholder for /blogs')
-  }
+    const user = await User.findById(req.user.id);
+    const blogs = await Blog.find({});
+    res.render('blogs/index', {
+      blogs,
+      user,
+      subTitle: '- Your Blogs'
+    });
+  },
 }
