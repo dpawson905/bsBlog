975720fc8dfb58bbec209e1b5d4c92bdc59bbd1a
Darrell Pawson <dpawson905@gmail.com>
2019-06-01 20:22:44 -0400

    bug fixes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,12 +25,12 @@ $(document).ready(function() {
   var $profileHeight = $(".profile-hero");
 
   $(document).scroll(function() {
-    if (url !== 'blog') {
+    
       $nav.toggleClass(
         "scrolled",
         $(this).scrollTop() > $navHeight.height() - 50 || $(this).scrollTop() > $profileHeight.height()
       );
-    }
+    
     $nav1.toggleClass("scrolled", $(this).scrollTop() > $nav1.height());
   });
 
