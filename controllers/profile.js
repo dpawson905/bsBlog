8641adaa202aab0ad5c4119b8d9e120eec51e22e
Darrell Pawson <dpawson905@gmail.com>
2019-07-14 16:27:09 -0400

    work on user profile and hide login and forgot password header links if user is on registration page

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,10 +1,13 @@
 const { cloudinary } = require('../cloudinary');
 const { deleteProfileImage } = require('../middleware');
 const User = require('../models/user');
+const Blogs = require('../models/blog');
 
 module.exports = {
   async getProfile(req, res, next) {
     let user = await User.findById(req.user.id);
-    res.render('profile/index', { user, url: 'profile', subTitle: '- Profile' });
+    let blogs = await Blogs.find({ 'author': req.user.id })
+    console.log(blogs)
+    res.render('profile/index', { blogs, user, url: 'profile', subTitle: '- Profile' });
   }
 }
