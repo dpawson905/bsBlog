6d51a8b89c7161acad14601e02d887644464aa06
Darrell Pawson <dpawson905@gmail.com>
2019-08-02 19:02:12 -0400

    work on followers

--- controllers/userDomain.js
+++ controllers/userDomain.js
@@ -3,7 +3,11 @@ const User = require('../models/user');
 module.exports = {
   async userIndex(req, res, next) {
     const user = await User.findOne({ username: req.vhost[0]});
-    res.send(user);
+    if (user) {
+      res.send('User found');
+    } else {
+      res.send('No User');
+    }
     console.log(req.vhost[0])
   }
 }
